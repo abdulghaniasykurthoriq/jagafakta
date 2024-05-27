ec15cfdf720be7c390a7c4ef65503f62f2fc104a
agat <abdulghaniasykurthoriq@gmail.com>
2024-05-27 14:32:29 +0700

    first commit

---
+++ src/controllers/predictHoaxController.js
@@ -0,0 +1,88 @@
+import multer from "multer";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage }).single("image");
+
+const predictHoaxController = {
+  predict: async (req, res) => {
+    upload(req, res, async (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: "error",
+          message: "Failed to upload image",
+        });
+      }
+
+      if (!req.file) {
+        return res.status(400).json({
+          status: "error",
+          message: "No image uploaded",
+        });
+      }
+
+      const { filename } = req.file;
+
+      // Mendapatkan informasi pengguna dari token
+      const Auth = req.userData;
+
+      try {
+        // Membuat prediksi baru
+        const predict = await prisma.predicts.create({
+          data: {
+            result: "fakta",
+            image_url: `http://localhost:5000/uploads/${filename}`,
+            userId: Auth.id, // Menggunakan id pengguna sebagai userId
+          },
+        });
+
+        return res.json({
+          status: "success",
+          message: "Image uploaded successfully",
+          filename: filename,
+          data: predict,
+        });
+      } catch (error) {
+        return res.status(500).json({
+          status: "error",
+          message: "Failed to create prediction",
+          error: error.message,
+        });
+      }
+    });
+  },
+
+  histories: async (req, res) => {
+    const Auth = req.userData;
+
+    try {
+      const predicts = await prisma.predicts.findMany({
+        where: {
+          userId: Auth.id,
+        },
+      });
+      return res.json({
+        message: "success",
+        data: predicts,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        status: "error",
+        message: "Failed to create prediction",
+        error: error.message,
+      });
+    }
+  },
+};
+
+export default predictHoaxController;
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bodyParser from "body-parser";
+import authRoutes from "./routes/authRoutes.js";
+import authMiddleware from "./middlewares/authMiddleware.js";
+import predictHoaxRoutes from "./routes/predictHoaxRoutes.js";
+import newsRoutes from "./routes/newsRoutes.js";
+
+const app = express();
+const PORT = 5000;
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/auth", authRoutes);
+app.use("/predict", authMiddleware, predictHoaxRoutes);
+app.use("/news", authMiddleware, newsRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
